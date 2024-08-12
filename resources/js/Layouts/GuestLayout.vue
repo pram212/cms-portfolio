6fefc819517f3e001f34941d5fd6f4ad7990bc5c
pramono <pramono6236@gmail.com>
2024-08-12 23:52:21 +0700

    styel: perbaikan logo

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -11,13 +11,13 @@ onMounted(() => {
   <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-cover" style="background-image: url('/img/bg-login.jpg');">
 
     <div class="w-full sm:max-w-md mt-6 px-6 pb-6 bg-base-300 shadow-md overflow-hidden sm:rounded-lg">
-      <Link href="/" class="w-full flex justify-center">
-        <div class="avatar">
+      <div class="w-full flex justify-center">
+        <Link href="/" class="avatar">
           <div class="w-40 rounded-full">
             <img src="/img/logo.svg" />
           </div>
-        </div>
-      </Link>
+        </Link>
+      </div>
 
       <slot />
 
